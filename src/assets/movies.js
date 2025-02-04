1a8bd932e85c48cdece1dffc33cb27ae38c4fdba
Beomjun_Kim <bj730098@naver.com>
2025-02-04 17:58:05 +0900

    Add files via upload

---
+++ src/assets/movies.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      title: "노량",
+      year: 2023,
+      category: "액션, 드라마",
+      textRed: "color: Red",
+      like: 0,
+      imgUrl: './assets/노량.jpg'
+    },
+    {
+      title: "아쿠아맨과 로스트 킹덤",
+      year: 2023,
+      category: "액션, 판타지, 어드벤처",
+      textRed: "color: Red",
+      like: 0,
+      imgUrl: './assets/아쿠아맨.webp'
+    },
+    {
+      title: "3일의 휴가",
+      year: 2023,
+      category: "판타지, 드라마",
+      textRed: "color: Red",
+      like: 0,
+      imgUrl: './assets/3일의 휴가.jpg'
+    },
+]
+
+  export default data
